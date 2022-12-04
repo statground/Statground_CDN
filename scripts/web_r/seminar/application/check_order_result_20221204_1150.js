eb1008a01e265c483023275d6996c595c441b39e
Jaeseong Yoo <praster1@gmail.com>
2022-12-04 11:49:30 +0900

    check_order_result_20221204_1150

---
+++ scripts/web_r/seminar/application/check_order_result_20221204_1150.js
@@ -0,0 +1,38 @@
+function check_order_result(orderID)
+{
+	$.ajax({
+		url : "/seminar/ajax_check_order_id/?orderID=" + orderID,
+		success:function(data){
+			$("#div_loader").hide()
+
+			if ( !Object.keys(data).includes('email') || !Object.keys(data).includes('username') || !Object.keys(data).includes('phone') ) {
+				$("#div_failed").show()
+
+			} else {
+				// 이미 수강신청이 완료된 이용자
+				if ("result" in data && "message" in data.result){
+					$("#div_infomation").show()
+
+					// 수강자 정보
+					html = "<br/>"
+					html += '<p id="email" class="card-text" style="color:black; font-size:small;"><b>E-mail:</b> ' + data.email + '</p>'
+					html += '<p id="username" class="card-text" style="color:black; font-size:small;"><b>이름:</b> ' + data.username + '</p>'
+					html += '<p id="phone" class="card-text" style="color:black; font-size:small;"><b>연락처:</b> ' + data.phone +'</p>'
+					html += "<br/>"
+					$("#div_userInfo").html(html)
+					
+					html = '<p>' + data.result.message + '</p>'
+					html += '<p>주문번호: ' + data.orderID + '</p>'
+					$("#div_buttons").html(html)
+					
+				// 확인 오류
+				} else {
+					$("#div_failed").show()
+				}
+			}
+
+		}, error : function(error) {
+			$("#div_failed").show()
+		},
+	})
+}
