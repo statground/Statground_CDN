aa4be7b852d047a47d8f361b316e615139e5120c
Jaeseong Yoo <praster1@gmail.com>
2022-12-06 22:52:28 +0900

    html_container_header

---
+++ scripts/web_r/html_form/html_container_header.js
@@ -0,0 +1,17 @@
+function html_container_header(title="", msg="")
+{
+	html = '<div class="container">'
+	html += '<div class="row justify-content-center">'
+	html += '<div class="col-lg-12">'
+	html += '<center>'
+	html += '<div style="padding: 35px;">'
+	html += '<h5 class="mb-4 text-primary font-secondary">' + title + '</h5>'
+	html += '<p>' + msg + '</p>'
+	html += '</div>'
+	html += '</center>'
+	html += '</div>'
+	html += '</div>'
+	html += '</div>'
+	
+	return html
+}
