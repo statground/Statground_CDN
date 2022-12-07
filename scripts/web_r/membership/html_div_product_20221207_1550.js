afbb083f62ce742b02162acaa390a2a92f4e4f63
Jaeseong Yoo <praster1@gmail.com>
2022-12-07 16:34:41 +0900

    html_div_product_20221207_1550

--- scripts/web_r/membership/html_div_product_20221207_1550.js
+++ scripts/web_r/membership/html_div_product_20221207_1550.js
@@ -17,12 +17,12 @@ function html_div_product(name="정회원", msg="", msg2="", price=100000, div_i
 	html += '</div>'
 	html += '<div id="div_buttons_payment_' + div_id.toString() + '" style="display:none;">'
 	html += html_button(color="warning", msg="카드 결제",
-						onclick='request_order_id(\'' + product_id + '\', \'card\', \'' + email + '\'')
+								onclick='request_order_id(\'' + product_id + '\', \'card\', \'' + email + '\'')
 	html += html_button(color="warning", msg="가상계좌",
-						onclick='request_order_id(\'' + product_id + '\', \'va\', \'' + email + '\'')
+								onclick='request_order_id(\'' + product_id + '\', \'va\', \'' + email + '\'')
 	html += '<br/>'
 	html += html_button(color="warning", msg="계좌이체",
-						onclick='request_order_id(\'' + product_id + '\', \'account\', \'' + email + '\'')
+								onclick='request_order_id(\'' + product_id + '\', \'account\', \'' + email + '\'')
 	//html += html_button(color="warning", msg="정기구독", disabled=true)
 	html += '</div>'
 	html += '</center>'
