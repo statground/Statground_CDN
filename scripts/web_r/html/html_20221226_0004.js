b87ddb38284e10af777e8db05c91bc0021ce7576
Jaeseong Yoo <praster1@gmail.com>
2022-12-26 00:06:09 +0900

    html_p

---
+++ scripts/web_r/html/html_20221226_0004.js
@@ -0,0 +1,51 @@
+// Container - Header
+function html_container_header(title="", msg="")
+{
+	html = '<div class="container">'
+	html += '<div class="row justify-content-center">'
+	html += '<div class="col-lg-12">'
+	html += '<center>'
+	html += '<div style="padding: 35px;">'
+	html += '<h5 class="mb-4 text-primary font-secondary">' + title + '</h5>'
+	html += '<p>' + msg + '</p>'
+	html += '</div>'
+	html += '</center>'
+	html += '</div>'
+	html += '</div>'
+	html += '</div>'
+	
+	return html
+}
+
+
+// Button
+function html_button(color="primary", msg = "", onclick=null, disabled=false)
+{
+	html = '<button type="button" class="btn btn-' + color + '" '
+	if (onclick != null) { html += 'onclick="' + onclick + '" '; }
+	if (disabled) { html += 'disabled ' }
+	html += '>'
+	html += msg
+	html += '</button>'
+	return html
+}
+
+
+// Card
+function html_card(title="", msg="")
+{
+	html = '<div class="card">'
+	html += '<div class="card-header" style="color:black;">' + title + '</div>'
+	html += '<div class="card-body">' + msg + '</div>'
+	html += '</div>'
+	
+	return html
+}
+
+
+// P
+function html_p(msg="", color="black")
+{
+	html = '<p style="color:' + color + '">' + msg = '</p>'
+	return html
+}
