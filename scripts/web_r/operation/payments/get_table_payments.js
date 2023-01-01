a7cdb3eed92e4a87aeca8ffd2fa1e6e92add8885
Jaeseong Yoo <praster1@gmail.com>
2023-01-01 20:55:43 +0900

    get_table_payments

---
+++ scripts/web_r/operation/payments/get_table_payments.js
@@ -0,0 +1,27 @@
+function get_table_payments()
+{
+	$.ajax({
+		url : "/operation/ajax_payments_table_payments",
+		success:function(data){
+			$('#loading_table_payments').remove()
+			table_payments_options = {
+				"dom": 'Bfrtip',
+				"buttons": ['copy', 'csv', 'excel', 'pdf', 'print'],
+				"ordering": true,
+				"bDestroy": true,
+				"lengthChange": true,
+				"searching": true,
+				"scrollX": true,
+				"lengthChange": true,
+				"lengthMenu":[5, 10, 20, 30, 40, 50],
+				"pageLength": 10,
+				columns: data.columns,
+				columnDefs: data.columnDefs,
+				data: data.data,
+				"oLanguage": { "sInfo" : "_TOTAL_명의 회원 중 _START_ ~ _END_번째" },
+				"language": { "lengthMenu": "페이지당 _MENU_ 개씩 보기" }
+			}
+			$('#table_payments').DataTable(table_payments_options);
+		}
+	})
+}
