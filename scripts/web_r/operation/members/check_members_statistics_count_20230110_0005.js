fc08b5915395b8be0430b76af0b362f3e03e50de
Jaeseong Yoo <praster1@gmail.com>
2023-01-10 00:05:29 +0900

    check_members_statistics_count

---
+++ scripts/web_r/operation/members/check_members_statistics_count_20230110_0005.js
@@ -0,0 +1,27 @@
+function check_members_statistics_count()
+{
+	$.ajax({
+		url : "/operation/ajax_members_statistics_count",
+		success:function(data){
+			html = ""
+			$("#div_members_statistics_count").html(html)
+
+			html += '<p style="text-align:center;">멤버 수</p>'
+			html += insert_div_block('총 가입자 수',
+									numberWithCommas(data.cnt_total) + '명')
+			html += insert_div_block('올해의 가입자 수',
+									numberWithCommas(data.cnt_current_year) + '명' 
+									+ '<br/>'
+									+ '(작년: ' + numberWithCommas(data.cnt_last_year) + '명)')
+			html += insert_div_block('이번 달 가입자 수',
+									numberWithCommas(data.cnt_current_month) + '명' 
+									+ '<br/>'
+									+ '(지난 달: ' + numberWithCommas(data.cnt_last_month) + '명)')
+			html += insert_div_block('오늘 가입자 수',
+									numberWithCommas(data.cnt_current_day) + '명' 
+									+ '<br/>'
+									+ '(어제: ' + numberWithCommas(data.cnt_last_day) + '명)')
+			$("#div_members_statistics_count").html(html)
+		}
+	})
+}
