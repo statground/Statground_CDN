3e7c1b99d7a6fdcde63300eb64e02d5822085285
Jaeseong Yoo <praster1@gmail.com>
2023-01-14 00:40:59 +0900

    chart_bar

---
+++ scripts/google_chart/chart_bar.js
@@ -0,0 +1,20 @@
+function chart_bar(inputData, divName, options)
+{
+	google.charts.load("current", {packages:['corechart']});
+	google.charts.setOnLoadCallback(drawChart);
+
+	function drawChart() {
+		var data = new google.visualization.arrayToDataTable(inputData);
+		var chart = new google.visualization.ColumnChart(document.getElementById(divName));
+  
+		chart.draw(data, options);
+		
+		$(window).smartresize(function () {
+			chart.draw(data, options);
+		});
+
+		$(window).onload(function () {
+			chart.draw(data, options);
+		});
+	}
+}
