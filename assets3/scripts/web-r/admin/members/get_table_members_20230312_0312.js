0af0830beede6fcafa191469c664f765a7a41904
Jaeseong Yoo <praster1@gmail.com>
2023-03-12 03:08:47 +0900

    get_table_members_20230312_0312

---
+++ assets3/scripts/web-r/admin/members/get_table_members_20230312_0312.js
@@ -0,0 +1,45 @@
+async function get_table_members(type){
+	const data = await fetch("/operation/ajax_members_table_members")
+	.then(res=> { return res.json(); })
+	.then(res=> { return res; });
+
+	let table_visitors_options = {
+		"dom": 'Bfrtip',
+		"buttons": ['copy', 'csv', 'excel', 'pdf', 'print'],
+		"ordering": true,
+		"bDestroy": true,
+		"lengthChange": true,
+		"searching": true,
+		"scrollX": true,
+		"scrollCollapse": true,
+		"lengthChange": true,
+		"lengthMenu":[5, 10, 20, 30, 40, 50],
+		"pageLength": 5,
+		columns: data.columns,
+		columnDefs: data.columnDefs,
+		data: data.data,
+		"order": [[ 5, 'desc' ]],
+		"oLanguage": { "sInfo" : "_TOTAL_명의 회원 중 _START_ ~ _END_번째" },
+		"language": { "lengthMenu": "페이지당 _MENU_ 개씩 보기" }
+	}
+
+	function Div_table(props)
+	{
+		return (
+			<div class="w-full max-w-full">
+				<div class="relative flex flex-col min-w-0 break-words bg-white border-0 shadow-soft-xl rounded-2xl bg-clip-border">
+					<div class="table-responsive">
+						<table id={props.id} class="w-full text-sm text-right text-gray-500 dark:text-gray-400">
+							<thead class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400"></thead>
+						</table>
+					</div>
+				</div>
+			</div>
+		)
+	}
+	
+	ReactDOM.render(<Div_table id="table_member_result" />, document.getElementById("table_members"))
+	$('#table_member_result').DataTable(table_visitors_options);
+}
+
+get_table_members()
