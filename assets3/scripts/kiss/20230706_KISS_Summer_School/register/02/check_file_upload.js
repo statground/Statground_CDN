7310e613f32d34352473607017c94735b3bb44f9
Jaeseong Yoo <praster1@gmail.com>
2023-06-04 00:31:33 +0900

    파일 업로드 체크

---
+++ assets3/scripts/kiss/20230706_KISS_Summer_School/register/02/check_file_upload.js
@@ -0,0 +1,3 @@
+function check_file_upload() {
+	ReactDOM.render(<Div_li_payment_btn />, document.getElementById("li_payment_btn"))
+}
---
+++ assets3/scripts/kiss/20230706_KISS_Summer_School/register/02/init_variables_20230604_0031.js
@@ -0,0 +1,16 @@
+// 강의 선택 여부
+let btn_days_1 = 0
+let btn_days_2 = 0
+
+// 가격
+let price_days_1 = 0
+let price_days_2 = 0
+
+// 학생 선택 여부
+let chk_student = 0
+
+let product_id = ""
+
+// 버튼 className
+const class_btn_enabled = "text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+const class_btn_disabled = "text-white bg-gradient-to-r from-purple-200 via-purple-300 to-purple-400 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 cursor-not-allowed"
