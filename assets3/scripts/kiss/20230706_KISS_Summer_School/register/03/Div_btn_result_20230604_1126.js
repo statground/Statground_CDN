da38ba0ea063dd95390833c86a89a2386967adf7
Jaeseong Yoo <praster1@gmail.com>
2023-06-04 11:27:05 +0900

    bug fix

---
+++ assets3/scripts/kiss/20230706_KISS_Summer_School/register/03/Div_btn_result_20230604_1126.js
@@ -0,0 +1,9 @@
+function Div_btn_result() {
+	return (
+		<button type="button" id="button_init"
+				class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 hidden"
+				onClick={() => location.href='/'}>
+			첫 화면으로
+		</button>        
+	)
+}
