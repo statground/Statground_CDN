c12eaca49243dc6f4caccdc729f2a6c07184ab50
Jaeseong Yoo <praster1@gmail.com>
2023-07-26 00:21:55 +0900

    회사 소개

---
+++ assets3/scripts/common/Div_header.js
@@ -0,0 +1,12 @@
+function Div_header() {
+	return (
+		<div class="flex flex-col justify-center items-center w-full">
+			<h1 class="mb-4 text-2xl font-extrabold text-gray-900 md:text-3xl lg:text-3xl">
+				<span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">국내 최대의 데이터 커뮤니티</span>
+			</h1>
+			<h1 class="mb-4 text-3xl font-extrabold text-gray-900 md:text-5xl lg:text-6xl">
+				<span>통계마당</span>
+			</h1>
+		</div>
+	)
+}
