8d59db36e6488252d9d17fabd74306cad29f371a
Jaeseong Yoo <praster1@gmail.com>
2023-07-26 01:42:54 +0900

    회사 소개

---
+++ assets3/scripts/common/Div_intro.js
@@ -0,0 +1,11 @@
+function Div_intro() {
+	return (
+		<div class="container mx-auto">
+			<Div_header />
+			<Div_our_story />
+			<Div_people />
+			<Div_partnership />
+			<Div_clients />
+		</div>
+	)
+}
