6ff4d144ae8270c0c1e11a4b4734bbc6c739ce85
Jaeseong Yoo <praster1@gmail.com>
2023-07-26 05:02:18 +0900

    Div_title

---
+++ assets3/scripts/web-r/admin/index/Div_title.js
@@ -0,0 +1,7 @@
+function Div_title(props) {
+	return (
+		<h5 class="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900">
+			<span class="text-blue-600">{props.title}</span>
+		</h5>            
+	)
+}
