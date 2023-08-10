5cbc54bfc17d8e9169d5a880b6076e6de2127931
Jaeseong Yoo <praster1@gmail.com>
2023-08-11 03:13:01 +0900

    테이블 출력

---
+++ assets3/scripts/admin/main/trigger/print_tbl_trigger.js
@@ -0,0 +1,39 @@
+async function print_tbl_trigger(mode) {
+	if (mode != "None") {
+		const data_request = new FormData();
+		data_request.append('mode', mode);
+
+		let data = await fetch("/main/ajax_trigger_view_get_trigger/", {  method: "post", 
+														headers: {"X-CSRFToken": getCookie("csrftoken")},
+														body: data_request
+													 })
+						.then(res=> { return res.json(); })
+						.then(res=> { return res; });
+
+	grid_trigger = new tui.Grid(
+		{
+			el: document.getElementById('tbl_trigger'),
+			data: Object.keys(data).map(function(element) {
+				return data[element]
+			}),
+			scrollX: false,
+			scrollY: false,
+			columns:  Object.keys(data['0']).map(function(element){
+				return {header:element, 
+						name:element, 
+						editor: {type: 'text' },
+						sortable: true,
+						width: 'auto'
+					   }
+			}),
+			columnOptions: {
+			  resizable: true
+			}
+		}
+	)
+
+	} else {
+		return false
+
+	}
+}
