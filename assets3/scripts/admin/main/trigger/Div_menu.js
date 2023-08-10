3f02038ea75c947d5af1a4de4e0e8eb6813cd2e6
Jaeseong Yoo <praster1@gmail.com>
2023-08-10 13:07:38 +0900

    Div_menu

---
+++ assets3/scripts/admin/main/trigger/Div_menu.js
@@ -0,0 +1,20 @@
+function Div_menu() {
+	return (
+		<section class="flex flex-col w-11/12 justify-center items-center mx-auto bg-white">
+			<ul class="max-w-md divide-y divide-gray-200">
+				<Div_sub_menu name="products" svg="https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/svg/product.svg" />
+				<Div_sub_menu name="database" svg="https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/svg/database.svg" />
+				<Div_sub_menu name="user_group" svg="https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/svg/user.svg" />
+				<Div_sub_menu name="user_group_WebR" svg="https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/svg/user.svg" />
+				<Div_sub_menu name="banner" svg="https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/svg/banner.svg" />
+				<Div_sub_menu name="toss_payments_key" svg="https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/svg/card.svg" />
+				<Div_sub_menu name="toss_payments_result_url" svg="https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/svg/card.svg" />
+				<Div_sub_menu name="github_api_key" svg="https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/svg/github.svg" />
+				<Div_sub_menu name="NAVER_ads_api_key" svg="https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/svg/naver_black.svg" />
+				<Div_sub_menu name="NAVER_api_key" svg="https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/svg/naver_black.svg" />
+				<Div_sub_menu name="facebook_active_group_list" svg="https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/svg/facebook_icon.svg" />
+				<Div_sub_menu name="facebook_columnist_list" svg="https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/svg/facebook_icon.svg" />
+			</ul>
+		</section>
+	)
+}
