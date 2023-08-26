0a2bfd11cdcab22446d32852d071dbedd9a4a7da
Jaeseong Yoo <praster1@gmail.com>
2023-08-26 15:26:28 +0900

    Threads

---
+++ assets3/scripts/common/footer/Div_footer_icons_20230826_1525.js
@@ -0,0 +1,30 @@
+function Div_footer_icons() {
+	return (
+		<div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0 w-full">
+			<a title="Facebook Group" class="text-gray-500 hover:text-gray-900"
+			href="https://www.facebook.com/groups/statground" target="_blank" alt="Facebook Group">
+				<img src="https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/svg/footer_facebook_group.svg" class="w-4 h-4" />
+			</a>
+			<a title="Facebook Page" class="text-gray-500 hover:text-gray-900"
+			href="https://www.facebook.com/Statground" target="_blank" alt="Facebook Page">
+				<img src="https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/svg/footer_facebook_page.svg" class="w-4 h-4" />
+			</a>
+			<a title="Twitter" class="text-gray-500 hover:text-gray-900"
+			href="https://twitter.com/Statground1" target="_blank" alt="Twitter">
+				<img src="https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/svg/footer_twitter_x.svg" class="w-4 h-4" />
+			</a>
+			<a title="Instagram" class="text-gray-500 hover:text-gray-900"
+			href="https://www.instagram.com/statground/" target="_blank" alt="Instagram">
+				<img src="https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/svg/footer_instagram.svg" class="w-4 h-4" />
+			</a>
+			<a title="Instagram" class="text-gray-500 hover:text-gray-900"
+			href="https://www.linkedin.com/company/82371650/" target="_blank" alt="LinkedIn Page">
+				<img src="https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/svg/footer_linkedin.svg" class="w-4 h-4" />
+			</a>
+			<a title="Threads" class="text-gray-500 hover:text-gray-900"
+			href="https://www.threads.net/@statground" target="_blank" alt="Threads">
+				<img src="https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/svg/footer_threads.svg" class="w-4 h-4" />
+			</a>
+		</div>
+	)
+}
