1d185194cde7e2b76600fe37583f63deb765ad9d
Jaeseong Yoo <praster1@gmail.com>
2023-09-30 20:13:36 +0900

    Tailwind CSS Config

---
+++ assets3/scripts/common/tailwind_config/tailwind_config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+	theme: {
+		screens: {
+			'xl': {'max': '1536px'},
+			'lg': {'max': '1280px'},
+			'md': {'max': '1024px'},
+			'sm': {'max': '768px'},
+			'xs': {'max': '640px'},
+		}
+	}
+}
