c527cfa282b1a24f3f51733704b1e38a017107b2
Jaeseong Yoo <praster1@gmail.com>
2023-11-14 12:58:21 +0900

    회사 소개 form 수정

---
+++ assets3/scripts/common/intro/init_variables.js
@@ -0,0 +1,202 @@
+let data_people = [
+	{
+		"uuid" : "6ba2918b-c91f-4213-9453-20af3542ea86",
+		"name" : "Jae-seong Yoo",
+		"role" : "CEO",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/member/JaeseongYoo.jpg",
+		"url_facebook" : "https://www.facebook.com/JSYoo86"
+	},
+	{
+		"uuid" : "c72f4bbd-e296-4f9f-9394-eb07bc49338e",
+		"name" : "Jae-kwang Kim",
+		"role" : "Technical Advisor",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/member/JaekwangKim.jpg",
+		"url_facebook" : "https://www.facebook.com/profile.php?id=100013068106711"
+	},
+	{
+		"uuid" : "697c5ba8-28a9-452d-abc9-cf1b180b5df0",
+		"name" : "Seung-sik Hwang",
+		"role" : "Admin. of Community",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/member/SeungsikHwang.jpg",
+		"url_facebook" : "https://www.facebook.com/seungsik.hwang"
+	},
+	{
+		"uuid" : "ecb4e762-d998-4646-bd67-feb9d021f9eb",
+		"name" : "Keon-Woong Moon",
+		"role" : "Admin. of Web-R",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/member/KeonwoongMoon.jpg",
+		"url_facebook" : "https://www.facebook.com/cardiomoon"
+	},
+	{
+		"uuid" : "a381e935-9a4b-4ce0-b5c2-995068ec0fc7",
+		"name" : "Jong-Heon Kim",
+		"role" : "Admin. of FuturEdu",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/member/JongheonKim.jpg",
+		"url_facebook" : "https://www.facebook.com/jongheon.kim.501"
+	}
+]
+
+let data_partnership = [
+	{
+		"uuid" : "f3e6d251-d2f0-470b-b2de-5bf54d6899e9",
+		"name" : "(주)KB국민카드",
+		"role" : "데이터 비즈(Data Biz) 파트너 계약",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/partnership/kbkookmincard.jpg",
+		"url_homepage" : "https://card.kbcard.com/"
+	},
+	{
+		"uuid" : "5a72f651-33de-4d8c-9aea-32cc2a132cbb",
+		"name" : "슬기로운 통계생활",
+		"role" : "사업 협력 계약",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/partnership/statisticsplaybook.jpg",
+		"url_homepage" : "https://statisticsplaybook.com/"
+	},
+	{
+		"uuid" : "0c3045d9-8479-4487-bd8c-3bde3135e3c3",
+		"name" : "(주)인사이트마이닝",
+		"role" : "공동 연구, 정보와 자료 교환, 장비와 인력 교류 등을 통한 업무 협약",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/partnership/insightmining.jpg",
+		"url_homepage" : "http://insightmining.co.kr/"
+	},
+	{
+		"uuid" : "242a7d5d-15d0-4feb-a108-f166c8561485",
+		"name" : "(사)AI프렌즈학회",
+		"role" : "기업 회원, 공동 세미나 진행",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/partnership/aifrenz.jpg",
+		"url_homepage" : "https://aifrenz.notion.site/"
+	},
+	{
+		"uuid" : "e6ffd8ec-6fd8-4277-93ba-6e0c85c75b73",
+		"name" : "(주)LG CNS",
+		"role" : "Data lake 포털 구축 등",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/partnership/LGCNS.jpg",
+		"url_homepage" : "https://www.lgcns.com/"
+	},
+	{
+		"uuid" : "2ccafbec-0b1d-4e1c-b842-f84bedf39969",
+		"name" : "이그나이트",
+		"role" : "Chief Technology Officer 참여 등",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/partnership/ignite-artist.jpg",
+		"url_homepage" : "https://www.ignite-artist.com/"
+	},
+	{
+		"uuid" : "1106e13f-8df7-4a69-835a-9123c4e39467",
+		"name" : "Korean International Statistical Society",
+		"role" : "2023 KISS Summer School 시행사 등",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/partnership/kiss.jpg",
+		"url_homepage" : "https://statkiss.org/"
+	},
+	{
+		"uuid" : "57cb4d3c-6126-47d7-b40a-947bcdeea728",
+		"name" : "Korea Startup Forum",
+		"role" : "코리아 스타트업 포럼 멘토단",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/partnership/koreastartupforum.jpg",
+		"url_homepage" : "https://kstartupforum.org/"
+	},
+	{
+		"uuid" : "df478140-4a9a-4f49-b9b1-2861ecfb6109",
+		"name" : "경북대학교 컴퓨터학부",
+		"role" : "경북대 온라인 현장실습 멘토링",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/partnership/knuit.jpg",
+		"url_homepage" : "https://computer.knu.ac.kr/"
+	},
+	{
+		"uuid" : "30502d82-43d4-4709-8ed2-e3c127fbf834",
+		"name" : "(주)워니프레임",
+		"role" : "공동 플랫폼 제작을 위한 업무 협약",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/partnership/wonyframe.jpg",
+		"url_homepage" : "http://wonyframe.com/"
+	},
+	{
+		"uuid" : "09e0c0a6-0d38-4a72-b763-4dfc6fc77feb",
+		"name" : "세종과학예술영재학교",
+		"role" : "교육통계분석 서비스 공동 개발",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/partnership/sasa.jpg",
+		"url_homepage" : "https://sasa.sjeduhs.kr/"
+	},
+	{
+		"uuid" : "f914fcb1-e01b-4196-b7a6-3e475017f0c1",
+		"name" : "소셜러스(주)",
+		"role" : "주식 양수 계약",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/partnership/socialerus.jpg",
+		"url_homepage" : "https://socialerus.com/"
+	}
+]
+
+let data_clients = [
+	{
+		"uuid" : "290b20d8-85aa-4e1e-b2c3-52de35e0235b",
+		"name" : "(주)바이풀디자인",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/client/by_fulldesign.jpg"
+	},
+	{
+		"uuid" : "ad73edaf-122c-4722-b068-4cd598ba1b01",
+		"name" : "한국환경연구원",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/client/kei.jpg"
+	},
+	{
+		"uuid" : "8dc26c2e-6d3d-4593-b231-523948297749",
+		"name" : "고려대학교",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/client/koreauniv.jpg"
+	},
+	{
+		"uuid" : "0e7b54de-b93f-4b41-8de3-417415fc28ad",
+		"name" : "성신여자대학교",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/client/sungshinuniv.jpg"
+	},
+	{
+		"uuid" : "1e334044-0c91-4d55-ba55-211cc7ad8edd",
+		"name" : "단국대학교천안캠퍼스",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/client/dankookuniv.jpg"
+	},
+	{
+		"uuid" : "1f5a6fa7-cd6d-4add-b39a-93fc32859c5d",
+		"name" : "한국에너지기술연구원",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/client/kier.jpg"
+	},
+	{
+		"uuid" : "266aa734-3070-4832-b174-9f1316072abe",
+		"name" : "충북대학교",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/client/chungbukuniv.jpg"
+	},
+	{
+		"uuid" : "8c10fc7c-7f8b-4085-a6ef-2aa017a81644",
+		"name" : "양산부산대학교병원",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/client/pnuyangsan.jpg"
+	},
+	{
+		"uuid" : "f97061e7-729f-45f7-87ff-12c25cf36f35",
+		"name" : "전남대학교병원",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/client/chonnamunivhospital.jpg"
+	},
+	{
+		"uuid" : "733acaca-80bb-4dc3-a7e4-d5fd654c7877",
+		"name" : "나무인텔리전스(주)",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/client/namu.jpg"
+	},
+	{
+		"uuid" : "bfbe287b-e324-4e1a-a602-e54f9afb2516",
+		"name" : "서울특별시 광역치매센터",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/client/seoulnid.jpg"
+	},
+	{
+		"uuid" : "796a3db1-d751-4ccd-8e43-74d63b457283",
+		"name" : "서울아산병원",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/client/asan.jpg"
+	},
+	{
+		"uuid" : "063f1d5e-658a-4428-8339-a3e38d4c45ac",
+		"name" : "조선대학교",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/client/chosun2.jpg"
+	},
+	{
+		"uuid" : "b3e1d6a0-e092-4083-9d5e-074d560baca8",
+		"name" : "JK통계컨설팅",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/client/jk.jpg"
+	},
+	{
+		"uuid" : "bdcb7d14-33ae-4234-b779-dc8fb35042c5",
+		"name" : "(주)DS이노베이션",
+		"url_image" : "https://cdn.jsdelivr.net/gh/statground/Statground_CDN/assets3/images/client/dsinnovation.jpg"
+	}
+]
