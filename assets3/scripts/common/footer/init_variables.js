59d24a96ad11eb277acc78015fd7ff9cdd0237b1
Jaeseong Yoo <praster1@gmail.com>
2023-12-09 13:41:59 +0900

    footer 스크립트 개편

---
+++ assets3/scripts/common/footer/init_variables.js
@@ -0,0 +1,9 @@
+let data_footer = {
+	"company": "주식회사 통계마당",
+	"representative": "대표, 개인정보보호책임자: 유재성",
+	"registration_no": "사업자등록번호: 795-88-02574",
+	"mail_order_no": "통신판매업신고번호: 2022-서울중구-2205",
+	"address": "서울특별시 중구 통일로 10 연세대학교세브란스빌딩 17층 1724호",
+	"phone": "대표전화: 0507-1300-9704",
+	"email": "문의: cs@statground.net",
+}
