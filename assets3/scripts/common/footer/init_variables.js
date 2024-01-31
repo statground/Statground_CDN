eb5ed0eff9a94fc345beb2e9c6c2a910e78a9b7c
Jaeseong Yoo <praster1@gmail.com>
2024-01-31 20:16:22 +0900

    주소 변경

--- assets3/scripts/common/footer/init_variables.js
+++ assets3/scripts/common/footer/init_variables.js
@@ -2,8 +2,8 @@ let data_footer = {
 	"company": "주식회사 통계마당",
 	"representative": "대표, 개인정보보호책임자: 유재성",
 	"registration_no": "사업자등록번호: 795-88-02574",
-	"mail_order_no": "통신판매업신고번호: 2022-서울중구-2205",
-	"address": "서울특별시 중구 통일로 10 연세대학교세브란스빌딩 17층 1724호",
+	"mail_order_no": "통신판매업신고번호: 2024-서울동작-0216",
+	"address": "서울특별시 동작구 동작대로7길 48, 새한빌딩 502호",
 	"phone": "대표전화: 0507-1300-9704",
 	"email": "문의: cs@statground.net",
 }
